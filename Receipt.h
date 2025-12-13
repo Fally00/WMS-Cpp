7e28e429bddb926d46d226254cc1d10061a19413
Rayo <rayanhisgam1234@gmail.com>
2025-12-14 00:17:54 +0200

    created receipt header file
    a vision for improving and scalling the wms to be a real system

---
+++ Receipt.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Item.h"
+
+
+// Structure to hold item details in the receipt
+struct ReceiptItem {
+    int id;
+    std::string name;
+    int quantity;
+};
+
+
+// Receipt class to manage purchased items
+class Receipt {
+    public:
+        void addItem(const Item& item, int quantity);
+        void print() const;
+        void clear();
+        void saveToFile(const std::string& filename) const;
+    private:
+        std::vector<ReceiptItem> items;
+
+
+    };
